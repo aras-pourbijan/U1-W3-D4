938ed423adbc9a57d754edab29c3147828be62b7
Aras Pourbijan <arasmidos@gmail.com>
2022-09-15 17:07:44 +0200

    manca funzione per numeri

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let primaStringa = "Cera Una Volta !";
+let upperCase = primaStringa.toUpperCase();
+let lowerCase = primaStringa.toLowerCase();
+
+console.log(upperCase)      //CERA UNA VOLTA !
+
+console.log(lowerCase)     // cera una volta !
+
+// ----------------------------------
+
+let table = "90536"
+
+let suddivisone= table/table.length 
+
+console.log(suddivisone)   // 18107.2
+
+
+
+let primoNumero=table[0];
+let secondoNumero=table[4];
+console.log(primoNumero+secondoNumero)  //96
+
+// ----------------------------------------
+
+let epicodeDevelopers=['aras', 'jovanni', 'antonio', 'luca', 'dimitri', 'gaia' , 'ciro'];
+
+let listItems =console.log(epicodeDevelopers)
+
+let terzoElemento= (epicodeDevelopers[2])
+console.log(terzoElemento);
+
+// ---------------------------------------------
+
+epicodeDevelopers.push("Lidia"); 
+let newPerson =epicodeDevelopers.length
+console.log("con nuova persona siamo:"+ newPerson )  
+// ------------------------------------------------
+
+let mixList=['hello', 4,6,'night', 'monday', 6, 2, 'chps', 1, '3']
+console.log(mixList)
+
+// let numberlist
+
+
